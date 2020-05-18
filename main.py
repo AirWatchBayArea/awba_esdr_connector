2b9abb3477100b535aab568da3b28988953db69f
sufyanAbbasi <sufy.abbasi@gmail.com>
2020-05-17 17:16:04 -0700

    Add to cron list

--- main.py
+++ main.py
@@ -70,10 +70,3 @@ app = webapp2.WSGIApplication([
 	('/valero', ValeroConnector),
 	('/fencelinerodeo', FencelineRodeoConnector),
 ], debug=True)
-
-def main():
-    from paste import httpserver
-    httpserver.serve(app, host='127.0.0.1', port='8080')
-
-if __name__ == '__main__':
-    main()
